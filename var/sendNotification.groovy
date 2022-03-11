99163ec6033fca6e7b85efc79c267233de9c5ca4
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-11 12:22:33 +0530

    Update sendNotification.groovy

--- var/sendNotification.groovy
+++ var/sendNotification.groovy
@@ -1,1 +1,14 @@
+def call(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'SUCCESS'
 
+  // Default values
+  def subject = "'Jenkins Pipeline ${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]"
+  def details = "${currentBuild.currentResult}: Job ${env.JOB_NAME} build ${env.BUILD_NUMBER}\n ${currentBuild.currentResult}: Job ${env.JOB_NAME} \n ${currentBuild.currentResult}: ${env.BUILD_TAG}"
+
+ emailext (
+      to: 'kumar.dk3671@gmail.com',
+      subject: subject,
+      body: details
+    )
+}
