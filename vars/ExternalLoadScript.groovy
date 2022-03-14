db203b35db86c12d1e9a97e9ea951beeb80833a8
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-14 11:13:41 +0530

    Update ExternalLoadScript.groovy

--- vars/ExternalLoadScript.groovy
+++ vars/ExternalLoadScript.groovy
@@ -1,5 +1,7 @@
 def call(Map config = [:]) {
+    
     loadScript(name: "Pythonzip.py")
-    loadScript(name: ""
-   
-}
+    loadScript(name: "sharelibjf-zip.py")
+    python "./Pythonzip.py"
+   }
+sh "./hello-world.sh ${config.name} ${config.dayOfWeek}"
