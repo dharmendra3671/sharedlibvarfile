c9f696be26d5349fec7958da5a8842d801c07722
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-14 10:53:32 +0530

    Update and rename loadPythonScript.groovy to loadScript.groovy

--- vars/loadScript.groovy
+++ vars/loadScript.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) { 
-  def scriptcontents = libraryResource "com/planetpope/scripts/linux/${config.name}"    
+  def scriptcontents = libraryResource "${config.name}"    
   writeFile file: "${config.name}", text: scriptcontents 
 } 
