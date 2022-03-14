329fe1220828f588f4ac7076aba886b4550d6e27
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-14 12:46:39 +0530

    Update ExternalLoadScript.groovy

--- vars/ExternalLoadScript.groovy
+++ vars/ExternalLoadScript.groovy
@@ -1,6 +1,7 @@
 def call(Map config = [:]) {
-    def filelst=["Pythonzip.py","sharelibjf-zip.py"]
-    for i in filelst:
-        loadScript(name:i)
-        python "./i${config.name}"
+    //def filelst=["Pythonzip.py","sharelibjf-zip.py"]
+    loadScript(name:"Pythonzip.py")
+    python "./Pythonzip.py ${config.name}"
+    loadScript(name:"sharelibjf-zip.py")
+    python "./sharelibjf-zip.py ${config.name}"
     }
