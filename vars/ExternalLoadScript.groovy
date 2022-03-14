b48451ce8fc70fa96928c9ce0289b5e720162418
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-14 13:21:34 +0530

    Update ExternalLoadScript.groovy

--- vars/ExternalLoadScript.groovy
+++ vars/ExternalLoadScript.groovy
@@ -1,7 +1,7 @@
 def call(Map config = [:]) {
     //def filelst=["Pythonzip.py","sharelibjf-zip.py"]
     loadScript(name:"Pythonzip.py")
-    python "./Pythonzip.py ${config.name}"
+    bat "./Pythonzip.py ${config.name}"
     loadScript(name:"sharelibjf-zip.py")
-    python "./sharelibjf-zip.py ${config.name}"
+    bat "./sharelibjf-zip.py ${config.name}"
     }
