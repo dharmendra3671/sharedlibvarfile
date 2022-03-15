0fbd93fc86070a7c6e613b3a68aca09c4050a53a
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-15 22:50:10 +0530

    Update get_resource_dir.groovy

--- vars/get_resource_dir.groovy
+++ vars/get_resource_dir.groovy
@@ -1,7 +1,6 @@
-def get_resource(){
-    def pythonContent = libraryResource('zipjf.py')
-    writeFile (file: 'zipjf.py', text: pythonContent)
-    bat "python zipjf.py"
+ def runMyPython(){
+     final pythonContent = libraryResource('zipjf.py')
+     writeFile (file: 'zipjf.py', text: pythonContent)
+     bat "python zipjf.py"
 }
 
-return this
