13ea2b8fd3868a20b4c9a9c8f5fab505a91791db
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-15 20:58:37 +0530

    Update sendNotification.groovy

--- vars/sendNotification.groovy
+++ vars/sendNotification.groovy
@@ -12,3 +12,17 @@ def call(String buildStatus = 'STARTED') {
       body: details
     )
 }
+
+import groovy.transform.SourceURI
+import java.nio.file.Path
+import java.nio.file.Paths
+
+class ScriptSourceUri {
+    @SourceURI
+    static URI uri
+}
+
+def call() {
+    Path scriptLocation = Paths.get(ScriptSourceUri.uri)
+    return scriptLocation.getParent().getParent().resolve('resources/Scripts').toString()
+}
