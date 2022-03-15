e6a7a7d0f86ca1c11bacee7239906500fb493b3d
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-15 13:15:19 +0530

    Delete ExternalLoadScript.groovy

--- vars/ExternalLoadScript.groovy
+++
@@ -1,7 +0,0 @@
-def call(Map config = [:]) {
-    //def filelst=["Pythonzip.py","sharelibjf-zip.py"]
-    loadScript(name:"Pythonzip.py")
-    bat "./Pythonzip.py ${config.name}"
-    loadScript(name:"sharelibjf-zip.py")
-    bat "./sharelibjf-zip.py ${config.name}"
-    }
