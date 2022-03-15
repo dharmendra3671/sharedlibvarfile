03b266b3e1f1896aa962366f51af5007090f043d
dharmendra3671 <89204055+dharmendra3671@users.noreply.github.com>
2022-03-15 23:52:15 +0530

    Update get_resource_dir.groovy

--- vars/get_resource_dir.groovy
+++ vars/get_resource_dir.groovy
@@ -6,4 +6,12 @@
      writeFile (file: 'artifact_jf.json', text: jsonContent)
      bat "python sharelibjf-zip.py"
 }
+def runpythonScripts(){
+     final pythonContent = libraryResource('Pythonzip.py')
+     writeFile (file: 'Pythonzip.py', text: pythonContent)
+  
+     final jsonContent = libraryResource('config.json')
+     writeFile (file: 'config.json', text: jsonContent)
+     bat "python Pythonzip.py"
+}
 
